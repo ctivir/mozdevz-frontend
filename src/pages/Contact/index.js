9f3b568a054a2f94e227b8d91935131a54a53571
Acrisio de Jesus <acrisiodejesus@gmail.com>
2021-12-27 19:53:31 +0200

    styled-components contact page

--- src/pages/Contact/index.js
+++ src/pages/Contact/index.js
@@ -1,4 +1,4 @@
-import styles from '../../styles/Contact.module.css'
+import { ContactPage } from './styles'
 import Header from '../../components/Header';
 import Footer from '../../components/Footer';
 import location from '../../assets/icons/location.svg'
@@ -11,69 +11,69 @@ import facebook from '../../assets/icons/facebook.svg'
 
 function Contact() {
   return (
-    <div className={styles.contact}>
+    <ContactPage>
       <Header />
       {/* Contact Container */}
-      <div className={styles.contactContainer}>
-        <aside className={styles.contactDetails}>
+      <div className="contactContainer">
+        <aside className="contactDetails">
           {/* Contact Description */}
-          <div className={styles.contactDescription}>
-            <h1 className={styles.contactTitle}>
+          <div className="contactDescription">
+            <h1 className="contactTitle">
               Contacte-nos
             </h1>
-            <p className={styles.contactText}>
+            <p className="contactText">
               Para mais informações não exite em escrever-nos
             </p>
           </div>
 
           {/* Contact Info */}
-          <div className={styles.contactInfo}>
-            <div className={styles.contactLabel}>
+          <div className="contactInfo">
+            <div className="contactLabel">
               <img
-                className={styles.icon}
+                className="icon"
                 src={phone}
                 alt="phone"
               />
-              <span className={styles.text}>
+              <span className="text">
                 +258 84 623 9715 / +258 84 729 8029
               </span>
             </div>
 
-            <div className={styles.contactLabel}>
+            <div className="contactLabel">
               <img
-                className={styles.icon}
+                className="icon"
                 src={email}
                 alt="email"
               />
-              <span className={styles.text}>
+              <span className="text">
                 info@mozdevz.org
               </span>
             </div>
 
-            <div className={styles.contactLabel}>
+            <div className="contactLabel">
               <img
-                className={styles.icon}
+                className="icon"
                 src={location}
                 alt="location"
               />
-              <span className={styles.text}>
+              <span className="text">
                 Av. Paulo Samuel Kankhomba, Nº 106 Maputo
               </span>
             </div>
           </div>
 
-          <div className={styles.contactSocials}>
-            <i className={styles.socialIcon}>
+          <div className="contactSocials">
+            <i className="socialIcon">
               <img
-                className={styles.icon}
+                className="icon"
                 src={facebook}
                 alt="facebook"
               />
             </i>
 
-            <i className={styles.socialIcon}>
+            <i className="socialIcon">
               <img
-                className={styles.icon}
+                className="icon"
                 src={twitter}
                 alt="twitter"
               />
@@ -82,17 +82,17 @@ function Contact() {
         </aside>
 
         {/* Contact Form */}
-        <aside className={styles.contactForm}>
-          <form className={styles.form}>
-            <div className={styles.input}>
+        <aside className="contactForm">
+          <form className="form">
+            <div className="input">
               <label htmlFor="nome">Seu nome</label>
               <input type="text" id="name" />
             </div>
-            <div className={styles.input}>
+            <div className="input">
               <label htmlFor="email">Seu email</label>
               <input type="email" id="email" />
             </div>
-            <div className={styles.input}>
+            <div className="input">
               <label htmlFor="message">Sua messagem</label>
               <textarea id="message" />
             </div>
@@ -101,7 +101,7 @@ function Contact() {
         </aside>
       </div>
       <Footer />
-    </div>
+    </ContactPage>
   )
 }
 
